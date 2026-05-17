a036199b56793b2348eeac1f9887e14091aef678
Marcus Leung <leungmarcus.email@gmail.com>
2026-05-16 23:49:21 -0700

    Connected the Plant Scan page to the AI API so it retrieves information about plant safety, edibility, and growth stage

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 require("dotenv").config();
-// require("./public/js/utils.js");
+require("./public/js/utils.js");
 
 const express = require("express");
 const session = require("express-session");
-// const MongoStore = require("connect-mongo").default;
+const MongoStore = require("connect-mongo").default;
 const app = express();
 const PORT = process.env.PORT || 2800;
 
@@ -27,70 +27,71 @@ const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_database = process.env.MONGODB_DATABASE;
-// const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 
-// const node_session_secret = process.env.NODE_SESSION_SECRET;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
 
-// const {signupSubmit, loginSubmit} = require("./public/js/authentication");
+const {signupSubmit, loginSubmit} = require("./public/js/authentication");
 const gameManager = require("./public/js/gameManager.js");
 
-// function checkAuthentication(req, res, next) {
-//     if (!req.session.authenticated) {
-//         res.redirect("/");
-//         return;
-//     }
-//     next();
-// }
+function checkAuthentication(req, res, next) {
+    if (!req.session.authenticated) {
+        res.redirect("/");
+        return;
+    }
+    next();
+}
 
-// var mongoStore = MongoStore.create({
-//     mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
-//     crypto: {
-//         secret: mongodb_session_secret,
-//     }
-// });
+var mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/${mongodb_database}`,
+    crypto: {
+        secret: mongodb_session_secret,
+    }
+});
 
-// app.use(session({
-//     secret: node_session_secret,
-//     store: mongoStore,
-//     saveUninitialized: false,
-//     resave: true
-// }));
+app.use(session({
+    secret: node_session_secret,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true
+}));
 
 app.get("/", (req, res) => {
+    if (req.session.authenticated) {
         res.redirect("/home");
         return;
-    
+    }
     res.redirect("/login");
 });
 
-// app.get("/signup", (req, res) => {
-//     res.render("signup");
-// });
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
 
-// app.post("/signupSubmit", signupSubmit);
+app.post("/signupSubmit", signupSubmit);
 
-// app.get("/login", (req, res) => {
-//     res.render("login");
-// });
+app.get("/login", (req, res) => {
+    res.render("login");
+});
 
-// app.post("/loginSubmit", loginSubmit);
+app.post("/loginSubmit", loginSubmit);
 
-app.get("/home", (req, res) => {
+app.get("/home", checkAuthentication, (req, res) => {
     let html = fs.readFileSync(__dirname + "/app/html/home.html", "utf8");
     res.send(html);
 });
 
-app.get("/plant-map", (req, res) => {
+app.get("/plant-map", checkAuthentication, (req, res) => {
     let html = fs.readFileSync(__dirname + "/app/html/plant-map.html", "utf8");
     res.send(html);
 });
 
 app.use("/plant-game", gameManager);
 
-// app.get("/logout", (req, res) => {
-//     req.session.destroy();
-//     res.redirect('/');
-// });
+app.get("/logout", (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 
 app.post('/api/scan', upload.single('plantImage'), async (req, res) => {
     try {
